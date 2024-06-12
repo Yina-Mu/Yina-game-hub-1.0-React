dafb8ba1a3ea513e66f62f748b7f8ad0bb2fd1af
Yina Mu <yinamu9@gmail.com>
2024-06-11 23:04:32 -0700

    Build the ColorMode Switched

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-import { HStack, Img, Text } from "@chakra-ui/react";
+import { ColorModeContext, HStack, Img, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
+import { Fragment } from "react/jsx-runtime";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Img src={logo} boxSize="60px" />
-      <Text>NavBar</Text>
-    </HStack>
+    <Fragment>
+      <HStack justifyContent="space-between" padding="10px">
+        <Img src={logo} boxSize="60px" />
+        <Text fontSize="20px">
+          <ColorModeSwitch />
+        </Text>
+      </HStack>
+    </Fragment>
   );
 };
 export default NavBar;
--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,22 @@ import ReactDOM from "react-dom/client";
 {
   /* #2 import third part libraries */
 }
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  ColorModeScript,
+} from "@chakra-ui/react";
 {
   /* #3 import own libraries */
 }
 import App from "./App";
+import theme from "./theme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
